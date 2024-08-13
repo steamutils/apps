dc11a33dbb855b5a65cffe9f4f8d0fcba63ffeb1
admin <admin@node.internal.scuffed.cloud>
2024-08-13 00:15:00 -0400

    fix script typos

--- apps/enshrouded/scripts/run.sh
+++ apps/enshrouded/scripts/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 source ./.enshrouded
-podman volume exists proton-$STEAMCMD_APPID || proton volume create proton-$STEAMCMD_APPID
-podman volume exists steamapp-$STEAMCMD_APPID || proton volume create steamapp-$STEAMCMD_APPID
+podman volume exists proton-$STEAMCMD_APPID || podman volume create proton-$STEAMCMD_APPID
+podman volume exists steamapp-$STEAMCMD_APPID || podman volume create steamapp-$STEAMCMD_APPID
 
 podman run -d --name $APP_NAME \
-	-e STEAMCMD_ARGS='+@sSteamCmdForcePlatformType windows +force_install_dir /steamapp +login anonymous +app_update 2278520 -public validate +quit'
+	-e STEAMCMD_ARGS='+@sSteamCmdForcePlatformType windows +force_install_dir /steamapp +login anonymous +app_update 2278520 -public validate +quit' \
 	-v steamapp-$STEAMCMD_APPID:/steamapp:z \
 	-v proton-$STEAMCMD_APPID:/proton:z \
 	-v ./enshrouded_server.json:/steamapp/enshrouded_server.json:z \
