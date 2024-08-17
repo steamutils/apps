7504ed10e3232014fbcce7b5d51270af5c0cbae7
Chris Kuperstein <ckuperst@redhat.com>
2024-08-16 22:43:10 -0700

    add docker scripts for the truly lazy

---
+++ enshrouded/docker-proton.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker run -d --name enshrouded \
+  -e STEAMCMD_ARGS='+@sSteamCmdForcePlatformType windows +force_install_dir /steamapp +login anonymous +app_update 2278520 -public validate +quit' \
+  -v steamapp-2278520:/steamapp:z \
+  -v ./enshrouded_server.json:/steamapp/enshrouded_server.json:z \
+  -p 15637:15637/udp \
+  quay.io/steamutils/runner:latest \
+  enshrouded_server.exe
