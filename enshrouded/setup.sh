00a2e559741108776054f8bc5f2e374ba471d5e9
Chris Kuperstein <ckuperst@redhat.com>
2024-09-12 08:15:15 -0700

    add config into configmap

--- enshrouded/setup.sh
+++ enshrouded/setup.sh
@@ -4,7 +4,6 @@ mkdir -p $HOME/.config/containers/systemd/enshrouded
 
 curl -sLJ https://raw.githubusercontent.com/steamutils/apps/main/enshrouded/deployment.yml >$HOME/.config/containers/systemd/enshrouded/deployment.yaml
 curl -sLJ https://raw.githubusercontent.com/steamutils/apps/main/enshrouded/enshrouded.kube >$HOME/.config/containers/systemd/enshrouded.kube
-curl -sLJ https://raw.githubusercontent.com/steamutils/apps/main/enshrouded/enshrouded_server.json >$HOME/.config/containers/systemd/enshrouded/enshrouded_server.json
 
 echo "Starting enshrouded through systemd podman quadlet"
 systemctl --user daemon-reload
@@ -17,4 +16,4 @@ echo "Wait for pod to run..."
 sleep 5
 
 echo -e "\nPod running. Check process logs with: \n\npodman logs -f enshrouded-pod-server"
-echo -e "Server Config file is located at $HOME/.config/enshrouded_server.json"
+echo -e "Server Config file is located in $HOME/.config/containers/systemd/enshrouded/deployment.yml"
